01c2f495653289bc62cded71ee0593502c09f03b
ItsAchance <alexander.chance@tutanota.com>
2025-10-06 11:06:33 +0200

    Moved to a selfmade structred logger and clean up from other refactoring

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,32 +2,28 @@ package main
 
 import (
 	"flag"
-	//"log"
 	"log/slog"
 	"net/http"
 	"os"
 )
 
-func main() {
+type application struct {
+	logger *slog.Logger
+}
 
-	addr := flag.String("addr", ":4000", "HTTP Network Port" )
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP Network Port")
 
 	flag.Parse()
-	
-	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	app := &application{
+		logger: logger,
+	}
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	logger.Info("Starting server on localhost", "Port", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
